5e7c87e8c84576ffee2d4267b966e5f2c02e6dca
TAYLOR MATHEUS BURGUES GOMES <u21452@LAPA19.academico.cotuca.unicamp.br>
2022-06-07 17:58:24 -0300

    primeira versao

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require ('cors');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extendend: true}))
+app.use(express.json());
+app.use(express.json({type: 'application/vnd.api+json'}));
+app.use(cors());
+
+const port = process.env.PORT ||3000;
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port,() => console.log(`Estou escutando na porta ${port}`))
